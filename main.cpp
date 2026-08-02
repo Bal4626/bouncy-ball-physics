91f6f454ca29470215f6e88527f90851967d97f3
Bal4626 <raniraj@hotmail.sg>
2026-08-02 20:55:33 +0800

    added gravity

--- main.cpp
+++ main.cpp
@@ -13,41 +13,54 @@ int main() {
     InitWindow(width, height, "Bouncy Ball Simulation"); 
 
     // start the position of the circle 
-    Vector2 position = {500.0f, 500.0f};
+    Vector2 position = {100.0f, 500.0f};
     // initialise the velocity for both the 2 dimensions
     // take note that Vector 2 is defined by 2 public float variables x and y
-    Vector2 velocity = {400.0f, 300.0f};
+    Vector2 velocity = {000.0f, -000.0f};
     float radius = 50.0f;
     float timer = 0.0f;
+    const float PIXELS_PER_METER = 100.0f;
+    const float gravity = 10.0f * PIXELS_PER_METER;
 
     //while the window is closed
     while (!WindowShouldClose()){
 
-        BeginDrawing();
-          
-        // draw the text from the top of the screen
-        DrawText(TextFormat("Let's Simulate!\nTime elapsed %.2f",GetTime()), 0, 0, 30, LIME);
+        // update the velocity for the gravity
+        velocity.y += gravity * GetFrameTime();
+
         // update the positon
         position.x += GetFrameTime()*velocity.x; 
         position.y += GetFrameTime()*velocity.y;
 
+        // the update phrase should be seperate from the render drawing
         // what happens if it hits the right side 
         if (position.x + radius >= GetScreenWidth()){
             velocity.x = velocity.x *-1;
+            // snapping the position
+            position.x = GetScreenWidth() - radius;
         }
         // left side
         if (position.x - radius <= 0){
             velocity.x = velocity.x *-1;
+            position.x = radius;
         }
         // top side
         if (position.y - radius <= 0){
             velocity.y = velocity.y *-1;
+            position.y = radius;
         }
         // bottom side
         if (position.y + radius >= GetScreenHeight()){
-            velocity.y = velocity.y *-1;
+            velocity.y *= -1;
+            float overshoot = (position.y + radius) - GetScreenHeight();
+            position.y = GetScreenHeight() - radius - overshoot;
         }
-        
+
+        BeginDrawing();
+          
+        // draw the text from the top of the screen
+        DrawText(TextFormat("Let's Simulate!\nTime elapsed %.2f\nVertical velocity = %.2f\nVertical position = %.2f" ,GetTime(),velocity.y, position.y), 0, 0, 30, LIME);
+
         // for changing the background colour
         timer += GetFrameTime();
         if ((int)timer % 2 == 0){
